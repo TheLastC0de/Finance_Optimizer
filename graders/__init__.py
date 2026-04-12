2a16a009fc796d935d1df5f595f8c8f9cab20687
TheLastC0de <rohanpendharkar30@gmail.com>
2026-04-12 16:34:35 +0530

    Fix fatal ImportError in graders.__init__ crashing server startup

--- graders/__init__.py
+++ graders/__init__.py
@@ -1,11 +1,9 @@
-from graders.all_graders import (
-    LedgerGrader,
-    SubscriptionGrader,
-    CashFlowGrader,
-    FraudGrader,
-    SavingsGrader,
-    DuplicateGrader
-)
+from graders.ledger_grader import LedgerGrader
+from graders.subscription_grader import SubscriptionGrader
+from graders.cash_flow_grader import CashFlowGrader
+from graders.fraud_grader import FraudGrader
+from graders.savings_grader import SavingsGrader
+from graders.duplicate_grader import DuplicateGrader
 
 __all__ = [
     "LedgerGrader", 
